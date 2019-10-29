2cb4823e7796153b891fe82257711c4342b7063e
tomimarkus991 <tomimarkusalber@gmail.com>
2019-10-29 09:26:45 +0200

    Removed unnecessary files

--- Programmid/X/X/Scripts/ListViewFolder/Weather/WeatherInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace X.Scripts
-{
-    class WeatherInfo
-    {
-        public string Name { get; set; }
-        public string Temperature { get; set; }
-        public string WindSpeed { get; set; }
-    }
-}
