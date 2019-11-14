194ce3ee0a618cb51a3c12a5230cedbfda2c1bbb
tomimarkus991 <tomimarkusalber@gmail.com>
2019-11-14 19:51:26 +0200

    Commented out WebView

--- Programmid/X/X/Scripts/WebView/XWebViewClient.cs
+++ Programmid/X/X/Scripts/WebView/XWebViewClient.cs
@@ -1,13 +1,13 @@
-﻿using Android.Webkit;
+﻿//using Android.Webkit;
 
-namespace X
-{
-    class XWebViewClient: WebViewClient
-    {
-        public override bool ShouldOverrideUrlLoading(WebView view, IWebResourceRequest request)
-        {
-            view.LoadUrl(request.Url.ToString());
-            return base.ShouldOverrideUrlLoading(view, request);
-        }
-    }
-}
+//namespace X
+//{
+//    class XWebViewClient: WebViewClient
+//    {
+//        public override bool ShouldOverrideUrlLoading(WebView view, IWebResourceRequest request)
+//        {
+//            view.LoadUrl(request.Url.ToString());
+//            return base.ShouldOverrideUrlLoading(view, request);
+//        }
+//    }
+//}
--- Programmid/X/X/Scripts/WebView/webView.cs
+++ Programmid/X/X/Scripts/WebView/webView.cs
@@ -1,35 +1,35 @@
-﻿using Android.App;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Webkit;
+﻿//using Android.App;
+//using Android.OS;
+//using Android.Runtime;
+//using Android.Views;
+//using Android.Webkit;
 
-namespace X
-{
-    [Activity(Label = "webView")]
-    public class webView : Activity
-    {
-        WebView _webView;
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
+//namespace X
+//{
+//    [Activity(Label = "WebView")]
+//    public class WebView : Activity
+//    {
+//        WebView _webView;
+//        protected override void OnCreate(Bundle savedInstanceState)
+//        {
+//            base.OnCreate(savedInstanceState);
 
-            // Create your application here
-            SetContentView(Resource.Layout.web_view);
+//            // Create your application here
+//            SetContentView(Resource.Layout.web_view);
 
-            var _webView = FindViewById<WebView>(Resource.Id.webView1);
-            _webView.Settings.JavaScriptEnabled = true;
-            _webView.SetWebViewClient(new XWebViewClient());
-            _webView.LoadUrl("https://www.neti.ee");
-        }
-        public override bool OnKeyDown([GeneratedEnum] Keycode keyCode, KeyEvent e)
-        {
-            if (keyCode == Keycode.Back && _webView.CanGoBack())
-            {
-                _webView.GoBack();
-                return true;
-            }
-            return base.OnKeyDown(keyCode, e);
-        }
-    }
-}
+//            var _webView = FindViewById<WebView>(Resource.Id.webView1);
+//            _webView.Settings.JavaScriptEnabled = true;
+//            _webView.SetWebViewClient(new XWebViewClient());
+//            _webView.LoadUrl("https://www.neti.ee");
+//        }
+//        public override bool OnKeyDown([GeneratedEnum] Keycode keyCode, KeyEvent e)
+//        {
+//            if (keyCode == Keycode.Back && _webView.CanGoBack())
+//            {
+//                _webView.GoBack();
+//                return true;
+//            }
+//            return base.OnKeyDown(keyCode, e);
+//        }
+//    }
+//}
