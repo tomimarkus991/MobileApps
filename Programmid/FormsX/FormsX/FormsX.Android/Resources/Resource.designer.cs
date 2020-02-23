4f49a65710f75970d92a2252cf6349bd675db423
tomimarkus991 <tomimarkusalber@gmail.com>
2020-02-23 12:30:42 +0200

    Added Instagram page

--- Programmid/FormsX/FormsX/FormsX.Android/Resources/Resource.designer.cs
+++ Programmid/FormsX/FormsX/FormsX.Android/Resources/Resource.designer.cs
@@ -7781,13 +7781,16 @@ namespace FormsX.Droid
 			public const int Prev = 2131165302;
 			
 			// aapt resource value: 0x7F070077
-			public const int tooltip_frame_dark = 2131165303;
+			public const int test = 2131165303;
 			
 			// aapt resource value: 0x7F070078
-			public const int tooltip_frame_light = 2131165304;
+			public const int tooltip_frame_dark = 2131165304;
 			
 			// aapt resource value: 0x7F070079
-			public const int Up = 2131165305;
+			public const int tooltip_frame_light = 2131165305;
+			
+			// aapt resource value: 0x7F07007A
+			public const int Up = 2131165306;
 			
 			static Drawable()
 			{
---
+++ Programmid/FormsX/FormsX/FormsX/ViewModels/PicturesIGViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace FormsX.ViewModels
+{
+    class PicturesIGViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ObservableCollection<Picture> Pictures { get; set; }
+        public PicturesIGViewModel()
+        {
+            Pictures = new ObservableCollection<Picture>
+            {
+                new Picture() { PictureUrl = "Resources", Title = "Minu pilt 1", Date = "20.08.2017" },
+                new Picture() { PictureUrl = "test.png", Title = "Minu pilt 2", Date = "20.08.2018" },
+                new Picture() { PictureUrl = "test.png", Title = "Minu pilt 3", Date = "20.08.2019" }
+            };
+        }
+        public class Picture
+        {
+            public string PictureUrl { get; set; }
+            public string Title { get; set; }
+            public string Date { get; set; }
+        }
+    }
+}
