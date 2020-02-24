8919be429e9b94c83ebee43be71f2c661b6158aa
tomimarkus991 <tomimarkusalber@gmail.com>
2020-02-24 20:42:57 +0200

    Finished Instagram App

--- Programmid/FormsX/FormsX/FormsX/ViewModels/PicturesIGViewModel.cs
+++ Programmid/FormsX/FormsX/FormsX/ViewModels/PicturesIGViewModel.cs
@@ -15,14 +15,29 @@ namespace FormsX.ViewModels
         }
 
         public ObservableCollection<Picture> Pictures { get; set; }
+
         public PicturesIGViewModel()
         {
-            Pictures = new ObservableCollection<Picture>
+
+
+            Pictures = new ObservableCollection<Picture>();
+            for (int i = 0; i < 30; i++)
             {
-                new Picture() { PictureUrl = "Resources", Title = "Minu pilt 1", Date = "20.08.2017" },
-                new Picture() { PictureUrl = "test.png", Title = "Minu pilt 2", Date = "20.08.2018" },
-                new Picture() { PictureUrl = "test.png", Title = "Minu pilt 3", Date = "20.08.2019" }
-            };
+                Random gen = new Random();
+                DateTime start = new DateTime(1995, 1, 1, 10, 40, 20);
+                int range = (DateTime.UtcNow - start).Days;
+                var date = start.AddDays(gen.Next(range)).AddHours(gen.Next(range)).ToString();
+                Random rnd = new Random();
+                var id = rnd.Next(1, 400);
+                var fakeTitle = Faker.Lorem.Sentence(2);
+                Pictures.Add(new Picture()
+                {
+                    PictureUrl = "https://i.picsum.photos/id/" + id + "/500/360.jpg",
+                    Title = fakeTitle,
+                    Date = date
+                });
+            }
+
         }
         public class Picture
         {
